d8a27b7b3222d8c30bf9825f1639033f2fbe74fc
user <user@this.com>
2022-08-17 11:28:50 -0400

    Initial Commit

---
+++ teamCDmccauthservice/src/main/java/com/bah/msd/mcc/TeamCDmccauthserviceApplication.java
@@ -0,0 +1,13 @@
+package com.bah.msd.mcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamCDmccauthserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamCDmccauthserviceApplication.class, args);
+	}
+
+}
